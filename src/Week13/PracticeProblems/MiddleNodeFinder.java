dd670b32b8a8e20d5e6d910e623f7d4f48f9aff8
Yash Bharuntia <bharuntiayash@gmail.com>
2025-11-05 23:30:38 +0530

    week 13 practice, lab and assignment problems

---
+++ src/Week13/PracticeProblems/MiddleNodeFinder.java
@@ -0,0 +1,36 @@
+package week13.practiceProblems;
+
+public class MiddleNodeFinder {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+        Node(int d) { data = d; next = null; }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) head = newNode;
+        else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    public void findMiddle() {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println("Middle Element: " + slow.data);
+    }
+
+    public static void main(String[] args) {
+        MiddleNodeFinder list = new MiddleNodeFinder();
+        list.insert(10); list.insert(20); list.insert(30); list.insert(40); list.insert(50);
+        list.findMiddle();
+    }
+}
---
+++ src/Week13/PracticeProblems/PhotoViewer.java
@@ -0,0 +1,51 @@
+package week13.practiceProblems;
+
+public class PhotoViewer {
+    class Node {
+        String photo;
+        Node next, prev;
+        Node(String p) { photo = p; }
+    }
+
+    Node head, tail, current;
+
+    public void addPhoto(String p) {
+        Node newNode = new Node(p);
+        if (head == null) head = tail = current = newNode;
+        else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void nextPhoto() {
+        if (current != null && current.next != null) {
+            current = current.next;
+            System.out.println("Next → " + current.photo);
+        } else {
+            System.out.println("No next photo.");
+        }
+    }
+
+    public void prevPhoto() {
+        if (current != null && current.prev != null) {
+            current = current.prev;
+            System.out.println("Prev ← " + current.photo);
+        } else {
+            System.out.println("No previous photo.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PhotoViewer viewer = new PhotoViewer();
+        viewer.addPhoto("Pic1");
+        viewer.addPhoto("Pic2");
+        viewer.addPhoto("Pic3");
+
+        System.out.println("Current: Pic1");
+        viewer.nextPhoto(); 
+        viewer.nextPhoto(); 
+        viewer.prevPhoto(); 
+    }
+}
